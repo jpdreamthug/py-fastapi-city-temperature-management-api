71bc5f1e9c6f07f495c5cb798e5e09bc432b3323
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-28 23:07:47 +0300

    add dependency for common query params

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import SessionLocal
@@ -6,3 +9,9 @@ from app.core.db import SessionLocal
 async def get_db() -> AsyncSession:
     async with SessionLocal() as session:
         yield session
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+CommonsDep = Annotated[dict, Depends(common_parameters)]
